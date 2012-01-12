fdb2d563bc192803e75b37d5c0f4b4ecb82059fb
Wade Sakundiak <wade.sakundiak@uregina.ca>
2012-01-12 15:20:12 -0600

    Fix widow script

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {	
-	$("h1, h2, h3, h4, h5, h6, p").each(function() {
-	    var wordArray = $(this).text().split(" ");
-	    wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
-	    wordArray.pop();
-	    $(this).html(wordArray.join(" "));
-	});
+	var heading = '';
+	$('h1, h2, h3, h4, h5, h6').each(function(index) {
+	  heading  = $(this).text().replace(/ (\w+)$/,'&nbsp;$1');
+	  $(this).html(heading);
+	});  
 	
 	$("h1:contains('&'), h2:contains('&'), h3:contains('&'), h4:contains('&'), h5:contains('&')", document.body).contents().each(function() {
 		if( this.nodeType == 3 ) {
